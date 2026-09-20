3f870e1a16b4fae8a89db6b0ac8b1c8705623bc1
Matthieu <matthieu.golsenne@gmail.com>
2026-09-20 11:37:54 +0200

    test11

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
-    loader: 'akamai',
-    path: '',
     qualities: [50, 75, 100]
   },
-  assetPrefix: './',
 };
 
 export default nextConfig;
