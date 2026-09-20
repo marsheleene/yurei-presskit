61818df9d9956ff180520dda2f78624361695887
Matthieu <matthieu.golsenne@gmail.com>
2026-09-20 11:46:01 +0200

    test12

--- components/Presskit/ImageItem.tsx
+++ components/Presskit/ImageItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import ExportedImage from 'next-image-export-optimizer';
 import Link from 'next/link';
 
 function ImageItem(props:any) {
@@ -24,11 +25,11 @@ function ImageItem(props:any) {
       className={className}
       target="_blank"
     >
-      <Image 
+      <ExportedImage 
         src={props.display} 
         sizes={sizes}
         alt={props.alt} 
-        quality={quality}
+        // quality={quality}
         className="mx-auto"
         priority={props.large}
       />
