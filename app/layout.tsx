d9e17ebd2e9a9d623f97065be110999563afcb1a
Matthieu <matthieu.golsenne@gmail.com>
2026-09-20 23:39:11 +0200

    test35

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     description: descriptionPage,
     images: [
       {
-          url: "./images/yurei-keyart-logo-landscape-small.png",
+          url: "./images/yurei-keyart-logo-og.jpg",
           alt: "Yurei key art"
       }
     ],
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     images: [
       {
-          url: "./images/yurei-keyart-logo-landscape-small.png",
+          url: "./images/yurei-keyart-logo-og.jpg",
           alt: "Yurei key art"
       }
     ],
