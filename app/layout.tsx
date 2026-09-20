d6e42212a49892b09f6465221642b352dc0f1ff0
Matthieu <matthieu.golsenne@gmail.com>
2026-09-20 23:18:55 +0200

    test31

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const descriptionPage = "Yūrei is a psychological horror game set in a manga’
 export const metadata: Metadata = {
   title: titlePage,
   description: descriptionPage,
-  metadataBase: new URL("https://marsheleene.github.io/yurei-presskit/"),
+  metadataBase: new URL("https://marsheleene.github.io/"),
   openGraph: {
     title: titlePage,
     description: descriptionPage,
