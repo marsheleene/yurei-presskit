215c955ca8ffd39afb96b50c221e72037ed23715
Matthieu <matthieu.golsenne@gmail.com>
2026-09-21 01:55:00 +0200

    test38

--- components/Presskit/Images/Images.tsx
+++ components/Presskit/Images/Images.tsx
@@ -1,4 +1,5 @@
 import ImageItem from '@/Components/ImageItem'
+import Spacing from '@/Components/Spacing'
 
 import Screenshot01 from '@/Images/yurei-screenshot01.jpg'
 import Screenshot02 from '@/Images/yurei-screenshot02.jpg'
@@ -8,6 +9,10 @@ function Images() {
   return (
     <>
       <h2 id="images">Images</h2>
+
+      <a href="https://drive.google.com/uc?export=download&id=1NdwmVuTmaNcc6Sn6lKxVcZ47kpoIhkuo">Download all the assets</a>
+
+      <Spacing />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <ImageItem src={Screenshot01} filename="yurei-screenshot01.jpg" alt="Yurei screenshot" />
